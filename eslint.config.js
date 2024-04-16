2a71e0694ab39c52cbd1e4ac57c15aaf3628a784
Johann Schopplich <mail@johannschopplich.com>
2024-04-16 11:30:14 +0200

    chore: migrate to eslint v9

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,10 @@
 import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
 
-export default createConfigForNuxt()
+export default createConfigForNuxt({
+  features: {
+    tooling: true,
+  },
+  dirs: {
+    src: ['./playground'],
+  },
+})
