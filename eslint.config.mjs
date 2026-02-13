f59c0cf0bf2cf4cae2614abc1c2f33ff62f4e86f
Johann Schopplich <mail@johannschopplich.com>
2026-02-13 18:28:55 +0100

    chore: migrate to antfu ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,45 +1,9 @@
-import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
-import perfectionist from 'eslint-plugin-perfectionist'
+// @ts-check
+import antfu from '@antfu/eslint-config'
 
-export default createConfigForNuxt({
-  features: {
-    tooling: true,
-    stylistic: true,
-  },
-  dirs: {
-    src: ['./playground'],
+export default antfu({
+  pnpm: false,
+  rules: {
+    'no-console': 'off',
   },
 })
-  // Workaround for https://github.com/eslint/eslint/issues/19134
-  .append({
-    rules: {
-      '@typescript-eslint/no-unused-expressions': 'off',
-    },
-  })
-  .append({
-    plugins: {
-      perfectionist,
-    },
-    rules: {
-      'import/order': 'off',
-      'perfectionist/sort-exports': ['error', { order: 'asc', type: 'natural' }],
-      'perfectionist/sort-imports': ['error', {
-        groups: [
-          'type',
-          ['parent-type', 'sibling-type', 'index-type'],
-          'builtin',
-          'external',
-          ['internal', 'internal-type'],
-          ['parent', 'sibling', 'index'],
-          'side-effect',
-          'object',
-          'unknown',
-        ],
-        newlinesBetween: 'ignore',
-        order: 'asc',
-        type: 'natural',
-      }],
-      'perfectionist/sort-named-exports': ['error', { order: 'asc', type: 'natural' }],
-      'perfectionist/sort-named-imports': ['error', { order: 'asc', type: 'natural' }],
-    },
-  })
